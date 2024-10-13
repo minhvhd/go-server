838d5c71f676ee2bbfa858bf823a610d430d353b
minh.vo <dev.minhvo@gmail.com>
2024-10-13 14:54:49 +0700

    feat: context package

--- main.go
+++ main.go
@@ -1,45 +1,50 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
-	"net/http"
+	"time"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/hello" {
-		http.Error(w, "404 Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "405 Method Not Allowed", http.StatusMethodNotAllowed)
+func withTimeout(ctx context.Context) {
+	cancelObserver := make(chan bool)
+	go func() {
+		select {
+		case <-ctx.Done():
+			fmt.Println(ctx.Err())
+			cancelObserver <- true
+			return
+		}
+	}()
+
+	isCancel := <-cancelObserver
+	if isCancel {
+		close(cancelObserver)
 		return
 	}
 
-	fmt.Fprint(w, "Hello World!")
+	time.Sleep(10 * time.Second)
+	fmt.Println("End of Program")
 }
 
-func formHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Handler")
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseForm() err: %v", err)
-		return
+func setNameFromClient(ctx context.Context) {
+	if value := ctx.Value("username"); value != nil {
+		ctx := context.WithValue(ctx, "username_1", "Huy Tran")
+		getListUsername(ctx)
 	}
-	fmt.Fprintf(w, "Form: %#v", r.Form)
-	name := r.Form.Get("name")
-	address := r.Form.Get("address")
-	fmt.Fprintf(w, "Name: %s, Address: %s", name, address)
+}
+
+func getListUsername(ctx context.Context) {
+	huyTran := ctx.Value("username_1")
+	minhVo := ctx.Value("username")
+	fmt.Println("Get List Username", huyTran, minhVo)
 }
 
 func main() {
-	fileServer := http.FileServer(http.Dir("static"))
-	http.Handle("/", fileServer)
-	http.HandleFunc("/form", formHandler)
-	http.HandleFunc("/hello", helloHandler)
+	timeoutCtx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+	valueCtx := context.WithValue(context.Background(), "username", "Minh Vo")
+	fmt.Println("Starting..")
 
-	fmt.Println("Listening on port 8080")
-
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+	withTimeout(timeoutCtx)
+	setNameFromClient(valueCtx)
 }
