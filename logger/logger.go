113a193001c5624dfe1c0f8ab9745057b88bbc86
minh.vo <dev.minhvo@gmail.com>
2024-10-15 15:01:29 +0700

    feat: implement the logger

---
+++ logger/logger.go
@@ -0,0 +1,29 @@
+package logger
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"net/http"
+)
+
+const requestIDKey = 1
+
+func Println(ctx context.Context, msg string) {
+	id, ok := ctx.Value(requestIDKey).(int64)
+	if !ok {
+		fmt.Println("Could not get request ID")
+		return
+	}
+
+	fmt.Printf("[%d] %s\n", id, msg)
+}
+
+func Decorate(callBack http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := rand.Int63()
+		ctx := context.WithValue(r.Context(), requestIDKey, id)
+
+		callBack.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
